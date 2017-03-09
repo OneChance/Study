a65d344592c3021130babe59adbf8882633087f7
OneChance <zhouh08@hotmail.com>
2017-03-09 16:06:10 +0800

    v9

--- app/src/main/java/com/logic/mes/MyApplication.java
+++ app/src/main/java/com/logic/mes/MyApplication.java
@@ -16,7 +16,7 @@ public class MyApplication extends Application {
 
     private static Context context;
     public static ScanUtil scanUtil;
-    public static Integer VERSION = 8;
+    public static Integer VERSION = 9;
     private static List<Activity> mList = new LinkedList();
     public static boolean netAble = true;
     public static boolean offlineAble = false;
